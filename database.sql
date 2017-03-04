47d56004fef18d3015141e683e77c7c9a03c011d
Jonny Holupchinski <jonholup@gmail.com>
2017-03-04 11:48:37 -0600

    base file structure complete

---
+++ database.sql
@@ -0,0 +1,11 @@
+CREATE TABLE todo (
+    id SERIAL PRIMARY KEY,
+    task varchar(100),
+    complete boolean
+);
+
+INSERT INTO todo(task, complete)
+VALUES ('Touch toes', false),
+('Buy an otter', false),
+('Take out garbage', false),
+('Eat artichoke', false);
